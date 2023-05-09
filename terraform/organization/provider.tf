217aacc465c6fec50c313009acee34bdb2deec2e
federojas <frojas@itba.edu.ar>
2023-05-09 16:24:43 -0300

    var readded

--- terraform/organization/provider.tf
+++ terraform/organization/provider.tf
@@ -2,6 +2,7 @@
 provider "aws" {
     alias = "aws" 
 
+    region = var.aws_region
     shared_config_files     = ["~/.aws/config"]
     shared_credentials_files = ["~/.aws/credentials"]
     profile = "default"
