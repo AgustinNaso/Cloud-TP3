c2e8d4f86ec0a94d9ce1834ef82a2049442d54fa
Agustin Naso <anaso@itba.edu.ar>
2023-05-13 11:12:11 -0300

    feat: api gw as a custom-module

---
+++ terraform/modules/api-gw/main.tf
@@ -0,0 +1,28 @@
+resource "aws_api_gateway_rest_api" "this" {
+
+  name = "G3-API"
+
+  endpoint_configuration {
+    types = ["REGIONAL"]
+  }
+}
+
+resource "aws_api_gateway_deployment" "example" {
+  rest_api_id = aws_api_gateway_rest_api.example.id
+
+  //La idea de esto es q cuando cambia una lambda se redeploye el api gw
+  triggers = {
+    //en vez de {} poner hash de las lambda
+    redeployment = sha1(jsonencode("{}"))
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_api_gateway_stage" "this" {
+  deployment_id = aws_api_gateway_deployment.this.id
+  rest_api_id   = aws_api_gateway_rest_api.this.id
+  stage_name    = "api"
+}
